40fa44876b7607edac814fa7f420e10ee9ceb66b
Erin Graves <egraveshna16@gmail.com>
2020-02-12 10:22:08 -0800

    template

---
+++ docs/precache-manifest.8ca08e4ca479a5fad4a2cacba592b3a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a32791e3324d0c1209d652c6848fe38",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ab12a086d44a07e31c83",
+    "url": "/portfolio/static/css/main.c350ee45.chunk.css"
+  },
+  {
+    "revision": "f686e99a9b6a6c927b70",
+    "url": "/portfolio/static/js/2.4afbc96e.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/portfolio/static/js/2.4afbc96e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab12a086d44a07e31c83",
+    "url": "/portfolio/static/js/main.073bfce2.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/portfolio/static/media/header.4317be93.jpg"
+  }
+]);
